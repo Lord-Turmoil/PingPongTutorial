258050c589d75672b95e1d885778b4eb0b301b33
Lord Turmoil <tony-turmoil@qq.com>
2023-08-09 13:56:28 +0800

    feat: add basic Rectangle

--- PingPongTutorial/inc/Defines.h
+++ PingPongTutorial/inc/Defines.h
@@ -59,4 +59,35 @@ struct Vector
 
 using Coordinate = Vector;
 
+
+struct Rectangle
+{
+    union
+    {
+        Coordinate pos;
+        struct { double x; double y; };
+    };
+    double width;
+    double height;
+
+    Rectangle() : x(0.0), y(0.0), width(0.0), height(0.0) {}
+    Rectangle(double _x, double _y, double _width, double _height)
+        : x(_x), y(_y), width(_width), height(_height) {}
+
+    Coordinate GetCenter() const
+    {
+        return { x + width * 0.5, y + height * 0.5 };
+    }
+
+    Coordinate Intersect(const Rectangle& rect) const
+    {
+        return Coordinate();
+    }
+
+    bool Contains(const Coordinate& coord) const
+    {
+        return ((x < coord.x && coord.x < x + width) && (y < coord.y && coord.y < y + height));
+    }
+};
+
 #endif
